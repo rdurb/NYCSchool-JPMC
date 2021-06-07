b8da6bd7b175619687b679bdf77ffa68428e7091
Riley Durbin <riley.durbin@tcu.edu>
2021-06-07 14:45:56 -0500

    Initial Commit

---
+++ 20210607-RileyDurbin-NYCSchools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  20210607-RileyDurbin-NYCSchools
+//
+//  Created by Riley Durbin on 6/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 20210607-RileyDurbin-NYCSchools/_0210607_RileyDurbin_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0210607_RileyDurbin_NYCSchoolsApp.swift
+//  20210607-RileyDurbin-NYCSchools
+//
+//  Created by Riley Durbin on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct _0210607_RileyDurbin_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
